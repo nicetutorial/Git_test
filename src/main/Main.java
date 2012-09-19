9435f079d23125ec6a7903a69420b3b0ff9c6889
adopleb <adopleb@ubuntu.ubuntu-domain>
2012-09-19 12:45:21 +0200

    Ein paar Bugs eingebaut

--- src/main/Main.java
+++ src/main/Main.java
@@ -16,7 +16,7 @@ public class Main {
     public static void main(String args[])
     {
         JFrame frame = new Frame();
-        frame.setVisible(true);
+        frame.setVisible(tru;
         
     }
 }
