81877a5c716dcf4844e076c666f39215df746ef3
adopleb <adopleb@ubuntu.ubuntu-domain>
2012-09-19 12:34:29 +0200

    Initial Netbeans Commit

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author adopleb
+ */
+public class Main {
+    
+    public static void main(String args[])
+    {
+        System.out.println("Hello Git!");
+    }
+}
